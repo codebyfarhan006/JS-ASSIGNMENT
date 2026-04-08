abd4e0af1863478b03c8534d85ed9b6a747f18b3
Md Farhan Khan <codebyfarhan@gmail.com>
2026-04-08 22:47:48 +0530

    question5.js

--- question5.js
+++ question5.js
@@ -1,5 +1,3 @@
-// Question 5: Skipping Numbers
-
 var N = parseInt(prompt("Enter N:"));
 var seed = parseInt(prompt("Enter seed:"));
 
@@ -7,11 +5,9 @@ var divisor = seed + 2;
 var sum = 0;
 var m = 0;
 
-// Keep going until sum >= N
 while (sum < N) {
   m = m + 1;
 
-  // Only add m if it is NOT divisible by divisor
   if (m % divisor !== 0) {
     sum = sum + m;
   }
